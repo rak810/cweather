643d8a2697b6a34ded5ea6b3ab7b0609fd9e6573
rak810 <rak810@protonmail.ch>
2021-12-15 21:39:12 +0600

    Api client for apis created.

---
+++ ApiClient/OpenWeatherApiClient.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Net.Http;
+using DotNetEnv;
+using System.Threading.Tasks;
+using cweather.LocationData;
+using Newtonsoft.Json.Linq;
+
+namespace cweather.ApiClient
+{
+    public class OpenWeatherApiClient : IDisposable
+    {
+       private readonly HttpClient _httpClient;
+       private readonly string _owmApiToken;
+        private bool disposedValue;
+
+        public OpenWeatherApiClient(string apiKey)
+       {
+           _httpClient = new HttpClient();
+           _owmApiToken = apiKey;
+       }
+       public OpenWeatherApiClient()
+       {
+           _httpClient = new HttpClient();
+           _owmApiToken = Env.GetString("OWMapiToken");     
+       }
+
+        private Uri GenerateReqUrl(float Lat, float Long) => new Uri($"https://api.openweathermap.org/data/2.5/onecall?lat={Lat}&lon={Long}&units=metric&exclude=minutely&appid={_owmApiToken}");
+
+        public async Task<JObject> GetWeatherAsync(Location loc)
+        {
+            var reqUri = GenerateReqUrl(loc.Coord.Lat, loc.Coord.Long);
+
+            Console.WriteLine(reqUri);
+            JObject weatherObj = null;
+
+            try
+            {
+                 var response = await _httpClient.GetAsync(reqUri);
+
+                 if(response.IsSuccessStatusCode)
+                 {
+                     Console.WriteLine("Successful Request OP");
+                     var jsonStr = await response.Content.ReadAsStringAsync();
+                     var jsonDict = JObject.Parse(jsonStr);
+                     weatherObj = jsonDict;
+                    //  var res = jsonDict.SelectToken("features");
+                 }
+                 else 
+                 {
+                     Console.WriteLine($"Request Failed. {response.RequestMessage}");
+                 }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error in OPApiClient : {e.Message}");
+                throw;
+            }
+
+            return weatherObj;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~OpenWeatherApiClient()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ ApiClient/SimpleMapBoxApiClient.cs
@@ -0,0 +1,86 @@
+using System.Net.Http;
+using System.Net;
+using DotNetEnv;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace cweather.ApiClient
+{
+    public class SimpleMapBoxApiClient : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _mbApiToken;
+        private bool disposedValue;
+
+        public SimpleMapBoxApiClient(string apiKey)
+       {
+           _httpClient = new HttpClient();
+           _mbApiToken = apiKey;
+       }
+        public SimpleMapBoxApiClient()
+        {
+            _httpClient = new HttpClient();
+           _mbApiToken = Env.GetString("MBapiToken");  
+        }
+
+        private Uri GenerateReqUrl(string loc) => new Uri($"https://api.mapbox.com/geocoding/v5/mapbox.places/{WebUtility.UrlEncode(loc)}.json?access_token={_mbApiToken}");
+
+        public async Task<JToken> GetLatLongAsync(string loc)
+        {
+            var reqUri = GenerateReqUrl(loc);
+            Console.WriteLine(reqUri);
+            JToken locationObj = null;
+
+            try
+            {
+                 var response = await _httpClient.GetAsync(reqUri);
+
+                 if(response.IsSuccessStatusCode)
+                 {
+                     Console.WriteLine("Successful Request");
+                     var jsonStr = await response.Content.ReadAsStringAsync();
+                     var jsonDict = JObject.Parse(jsonStr);
+                     locationObj = jsonDict.SelectToken("features").First;
+                    //  var res = jsonDict.SelectToken("features");
+                 }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error in MapApiClient : {e.Message}");
+                throw;
+            }
+
+            return locationObj;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~SimpleMapBoxApiClient()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
