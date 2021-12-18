f1aff71516f02dc5e4a4255d29170e4f80ce0c16
rak810 <rak810@protonmail.ch>
2021-12-18 20:55:53 +0600

    Method table forecasting added

--- RenderWeatherData.cs
+++ RenderWeatherData.cs
@@ -1,6 +1,8 @@
 using Spectre.Console;
 using cweather.WeatherData;
 using cweather.LocationData;
+using System.Collections.Generic;
+using Spectre.Console.Rendering;
 
 namespace cweather
 {
@@ -19,7 +21,7 @@ namespace cweather
         {
             string locationDetail = $"[bold blue] Location:[/] {_location.PlaceName} [[{_location.Coord.Lat}, {_location.Coord.Long}]]";
             AnsiConsole.MarkupLine(locationDetail);
-            string timeDetail = $"[bold blue] Date:[/]{_weather.Current.Date.Date} [bold blue] Timezone:[/] {_weather.TimeZone}";
+            string timeDetail = $"[bold blue] Date:[/]{_weather.Current.Date} [bold blue] Timezone:[/] {_weather.TimeZone}";
             AnsiConsole.MarkupLine(timeDetail);
             string wDetail = $"[bold blue] Weather:[/] {_weather.Current.WDesc[0].Main} / {_weather.Current.WDesc[0].Description}";
             AnsiConsole.MarkupLine(wDetail);
@@ -48,13 +50,42 @@ namespace cweather
                 $"{_weather.Current.WFactors.Clouds} %",
                 $"{_weather.Current.WFactors.Visibility} m"
             );
-            // table.AddRow("Pressure", $"{_weather.Current.WFactors.Pressure}hPa");
-            // table.AddRow("Humidity", $"{_weather.Current.WFactors.Humidity}%");
-            // table.AddRow("Dew Point", $"{_weather.Current.WFactors.DewPoint}ºC");
-            // table.AddRow("UVI", $"{_weather.Current.WFactors.Uvi}");
-            // table.AddRow("Clouds", $"{_weather.Current.WFactors.Clouds}%");
-            // table.AddRow("Visibility", $"{_weather.Current.WFactors.Visibility}m");
             AnsiConsole.Write(table);
         }
+
+        public void ForecastHourlyWeather(int count)
+        {
+            var table = new Table();
+            table.Border(TableBorder.Rounded);
+            table.Title($"Forecast for next {count} days");
+            table.AddColumn(new TableColumn("Time").Centered());
+            table.AddColumn(new TableColumn("Temp (Feels Like)").Centered());
+            table.AddColumn(new TableColumn("Humidity").Centered());
+            table.AddColumn(new TableColumn("Wind").Centered());
+            table.AddColumn(new TableColumn("Description").Centered());
+
+            for(int i = 1; i < count; i++)
+            {
+                var row = RenderHourlyWeather(_weather.Hourly[i]);
+                table.AddRow(row);
+            }  
+
+            AnsiConsole.Write(table);
+        }
+
+        private static TableRow RenderHourlyWeather(HourlyWeather hourlyWeather)
+        {
+            
+            IEnumerable<IRenderable> wList = new List<IRenderable>{
+                new Markup($"{hourlyWeather.Date.Day}/{hourlyWeather.Date.Month} {hourlyWeather.Date.ToShortTimeString()}"),
+                new Markup($"{hourlyWeather.Temp}({hourlyWeather.FeelsLike}) ºC"),
+                new Markup($"{hourlyWeather.wFacts.Humidity} %"),
+                new Markup($"{hourlyWeather.wFacts.Wind.WindSpeed} m/s, ({hourlyWeather.wFacts.Wind.WindDeg}º)"),
+                new Markup($"{hourlyWeather.WDesc[0].Description.ToUpper()}")
+            };
+
+            return new TableRow(wList);
+        }
+
     }
 }
