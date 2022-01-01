add288c3a7f5d8bfec6a807cb37b83172646b101
rak810 <rak810@protonmail.ch>
2022-01-02 00:27:05 +0600

    Todo: Add commandline parsing

--- RenderWeatherData.cs
+++ RenderWeatherData.cs
@@ -3,6 +3,7 @@ using cweather.WeatherData;
 using cweather.LocationData;
 using System.Collections.Generic;
 using Spectre.Console.Rendering;
+using System;
 
 namespace cweather
 {
@@ -29,6 +30,18 @@ namespace cweather
             AnsiConsole.MarkupLine($"[bold blue] Feels Like:[/] {_weather.Current.FeelsLike}ºC");
             string windDetail = $"[bold blue] Wind:[/] {_weather.Current.WFactors.Wind.WindSpeed} m/s, {_weather.Current.WFactors.Wind.WindDeg}º";
             AnsiConsole.MarkupLine(windDetail);
+
+            if(_weather.Hourly[0].Rain != null)
+            {
+                string rainDetail = $"[bold blue] Rain:[/] {_weather.Hourly[0].Rain.HourOne} mm";
+                AnsiConsole.MarkupLine(rainDetail);
+            }
+
+            if(_weather.Hourly[0].Snow != null)
+            {
+                string snowDetail = $"[bold blue] Rain:[/] {_weather.Hourly[0].Snow.HourOne} mm";
+                AnsiConsole.MarkupLine(snowDetail);
+            }
         }
 
         public void CurrentWeatherDetail()
@@ -48,8 +61,9 @@ namespace cweather
                 $"{_weather.Current.WFactors.DewPoint}º C",
                 $"{_weather.Current.WFactors.Uvi}",
                 $"{_weather.Current.WFactors.Clouds} %",
-                $"{_weather.Current.WFactors.Visibility} m"
+                $"{_weather.Current.WFactors.Visibility??=0} m"
             );
+
             AnsiConsole.Write(table);
         }
 
@@ -57,7 +71,7 @@ namespace cweather
         {
             var table = new Table();
             table.Border(TableBorder.Rounded);
-            table.Title($"Forecast for next {count} days");
+            table.Title($"Forecast for next {count} hours");
             table.AddColumn(new TableColumn("Time").Centered());
             table.AddColumn(new TableColumn("Temp (Feels Like)").Centered());
             table.AddColumn(new TableColumn("Humidity").Centered());
@@ -87,5 +101,85 @@ namespace cweather
             return new TableRow(wList);
         }
 
+        public void ForecastDailyWeather(int i)
+        {
+            if(i > 6) {
+                throw new ArgumentOutOfRangeException(nameof(i), $"Value must be between 1 and 6");
+            }
+
+            string locationDetail = $"[bold blue] Location:[/] {_location.PlaceName} [[{_location.Coord.Lat}, {_location.Coord.Long}]]";
+            var table = new Table();
+            table.Border(TableBorder.Double);
+            table.Title = new TableTitle($"[bold blue] Forecast for {_weather.Daily[i].Date.ToLongDateString()}[/]");
+            table.AddColumn(new TableColumn(string.Empty));
+            table.HideHeaders();
+            table.AddRow(new Markup(locationDetail));
+            table.AddRow(new Markup($"[bold blue] Weather:[/] {_weather.Daily[i].WDesc[0].Main} / {_weather.Daily[i].WDesc[0].Description}"));
+            table.AddRow(new Markup($"[bold blue] Max/Min temp: [/] {_weather.Daily[i].Temp.Max}ºC / {_weather.Daily[i].Temp.Min}ºC"));
+            table.AddRow(new Markup($"[bold blue] Precipitation: [/] {_weather.Daily[i].Precipitation * 100}%"));
+            if(_weather.Daily[i].Rain != null)
+            {
+                table.AddRow(new Markup($"[bold blue] Rain: [/] {_weather.Daily[i].Rain} mm"));
+            }
+
+            if(_weather.Daily[i].Snow != null)
+            {
+                table.AddRow(new Markup($"[bold blue] Snow: [/] {_weather.Daily[i].Snow} mm"));
+            }
+            table.AddRow(RenderDailyTemp(_weather.Daily[i]));
+            table.AddRow(RenderDailyDetails(_weather.Daily[i]));
+            AnsiConsole.Write(table);
+            
+        }
+
+        private static Table RenderDailyDetails(DailyWeather dailyWeather)
+        {
+            var table = new Table();
+            table.Centered();
+            table.Title("Other Details");
+            table.AddColumn(new TableColumn("Pressure").Centered());
+            table.AddColumn(new TableColumn("Humidity").Centered());
+            table.AddColumn(new TableColumn("Dew Point").Centered());
+            table.AddColumn(new TableColumn("UVI").Centered());
+            table.AddColumn(new TableColumn("Clouds").Centered());
+            table.AddColumn(new TableColumn("Wind").Centered());
+
+            table.AddRow(
+                $"{dailyWeather.wFacts.Pressure} hPa",
+                $"{dailyWeather.wFacts.Humidity} %",
+                $"{dailyWeather.wFacts.DewPoint}º C",
+                $"{dailyWeather.wFacts.Uvi}",
+                $"{dailyWeather.wFacts.Clouds} %",
+                $"{dailyWeather.wFacts.Wind.WindSpeed} m/s, ({dailyWeather.wFacts.Wind.WindDeg}º)"
+            );
+            return table;
+        }
+
+
+        private static Table RenderDailyTemp(DailyWeather dailyWeather)
+        {
+            var table = new Table();
+            table.Title("Temperatures Across All Day");
+            table.AddColumn(new TableColumn("Morning").Centered());
+            table.AddColumn(new TableColumn("Day").Centered());
+            table.AddColumn(new TableColumn("Evening").Centered());
+            table.AddColumn(new TableColumn("Night").Centered());
+        
+            table.AddRow(new List<IRenderable>{
+               new Markup($"[blue] Temperature:[/] {dailyWeather.Temp.Morn} ºC"),
+               new Markup($"[blue] Temperature:[/] {dailyWeather.Temp.Day} ºC"),
+               new Markup($"[blue] Temperature:[/] {dailyWeather.Temp.Eve} ºC"),
+               new Markup($"[blue] Temperature:[/] {dailyWeather.Temp.Night} ºC")
+            });
+
+            table.AddRow(new List<IRenderable>{
+               new Markup($"[blue] Feels Like:[/] {dailyWeather.FeelsLike.Morn} ºC"),
+               new Markup($"[blue] Feels Like:[/] {dailyWeather.FeelsLike.Day} ºC"),
+               new Markup($"[blue] Feels Like:[/] {dailyWeather.FeelsLike.Eve} ºC"),
+               new Markup($"[blue] Feels Like:[/] {dailyWeather.FeelsLike.Night} ºC")
+            });
+            
+            return table;
+        }
     }
 }
