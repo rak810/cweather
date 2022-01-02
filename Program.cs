9952167c0206167943024f5f90db2f7f5fcb9ec2
rak810 <rak810@protonmail.ch>
2022-01-02 23:13:25 +0600

    UTF8 encoding added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
 using DotNetEnv;
 using Spectre.Console.Cli;
 using cweather.CommandLine;
@@ -10,8 +12,8 @@ namespace cweather
         static async Task Main(string[] args)
         {
             Env.Load();
+            Console.OutputEncoding = Encoding.UTF8;
             var app = new CommandApp();
-
             app.Configure(config =>
             {
                 config.AddBranch<ForecastSettings>("fcast", fcast =>
@@ -33,3 +35,24 @@ namespace cweather
 
 
 
+// using Spectre.Console;
+
+// Synchronous
+// await AnsiConsole.Status()
+//     .StartAsync("Thinking...", async ctx => 
+//     {
+//         ctx.Spinner(Spinner.Known.Circle);
+//         // Simulate some work
+//         AnsiConsole.MarkupLine("Doing some work...");
+//         await Task.Delay(3000);
+        
+//         // Update the status and spinner
+        
+//         ctx.Status("Thinking some more");
+
+//         // Simulate some work
+//         AnsiConsole.MarkupLine("Doing some more work...");
+//         await Task.Delay(4000);
+//     });
+
+// Console.WriteLine(10);
